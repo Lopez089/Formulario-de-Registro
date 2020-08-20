8b466b47970b2f11ff84874673755c274266365c
Juan <aragonlopezjuan87@gmail.com>
2020-08-20 17:22:59 +0200

    firebase

---
+++ build/precache-manifest.5689ae911b770a4dc2f914d11658ec11.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27d2011b6824c99bb7a9a6831e091e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f0eef5f4beabc1a2882",
+    "url": "/static/css/2.ab8a0683.chunk.css"
+  },
+  {
+    "revision": "0135a02943054b2393b5",
+    "url": "/static/css/main.1480fd6e.chunk.css"
+  },
+  {
+    "revision": "8f0eef5f4beabc1a2882",
+    "url": "/static/js/2.b3ce90d4.chunk.js"
+  },
+  {
+    "revision": "0135a02943054b2393b5",
+    "url": "/static/js/main.26cc062f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
