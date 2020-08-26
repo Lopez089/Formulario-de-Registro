b77291c92c39f6b9334b759ebafa4c47c8aab509
Juan <aragonlopezjuan87@gmail.com>
2020-08-26 19:41:38 +0200

    import only what is necessary from yup

--- src/service/validationForm.js
+++ src/service/validationForm.js
@@ -1,25 +1,25 @@
-import * as Yup from "yup";
+import { object, string, boolean, ref } from "yup";
 
-const validationSchema = Yup.object().shape({
-    name: Yup.string()
+const validationSchema = object().shape({
+    name: string()
         .min(2, "Escribe tu nombre")
         .max(25, "Debe de tener menos de 25 caracteres")
         .required("Debe de introducir tu nombre"),
-    Username: Yup.string()
+    Username: string()
         .min(2, "Escribe tu Username")
         .max(25, "Debe de tener menos de 25 caracteres")
         .required("Debe de introducir tu username"),
-    Email: Yup.string()
+    Email: string()
         .email("El correo electrónico debe ser válido")
         .required("Debe de introducir tu email"),
-    Password: Yup.string()
+    Password: string()
         .min(8, "Debe de tener al menos 8 caracteres")
         .max(15, "Debe de tener menos de 15 caracteres")
         .required("Debe de introducir tu Password"),
-    ConfirmPassword: Yup.string()
+    ConfirmPassword: string()
         .required("repita su contraseña")
-        .oneOf([Yup.ref("Password")], "Passwords must match"),
-    termsOfService: Yup.boolean()
+        .oneOf([ref("Password")], "Passwords must match"),
+    termsOfService: boolean()
         .oneOf([true], "accept the Terms of use & Privacy Policy")
         .required("Tiene que acptar los terminos y condiciones"),
 });
